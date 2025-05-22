214fe9f9bd578a8681e43d57ec23778a0de6a3bc
Cesar <cesarvillalobosolmos.01@gmail.com>
2025-05-22 12:33:43 -0600

    :sparkles: Init chat interface

--- support-chat-framework/app/routes.ts
+++ support-chat-framework/app/routes.ts
@@ -8,5 +8,12 @@ export default [
       route('login', 'routes/auth/login-page.tsx'),
       route('register', 'routes/auth/register-page.tsx'),
     ]),
+  ]),
+
+  ...prefix('/chat', [
+    layout('layouts/chat-layout.tsx', [
+      index('routes/chat/no-chat-selected-page.tsx'),
+      route(':id', 'routes/chat/client-chat-page.tsx'),
+    ])
   ])
 ] satisfies RouteConfig;
